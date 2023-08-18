86e395a28067af06d7186eb0c1ce5e72047213c2
ROSEMENESE <dianavillacis05@outlook.es>
2023-08-18 09:32:44 -0500

    el boton modifica la imagen

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,4 +1,7 @@
 saludar=function(){
+    let cmpImagenSaludo;
+    cmpImagenSaludo=document.getElementById("imgSaludo");
+    cmpImagenSaludo.src="./imagenes/ganamos-como-siempre-baile-feliz.gif";
     console.log("ingresa a saludar");
     
 }
