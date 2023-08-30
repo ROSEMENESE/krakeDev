d15d960e1fc51ef993fc0a4e14a088f7e5cd9e2f
ROSEMENESE <dianavillacis05@outlook.es>
2023-08-29 20:00:36 -0500

    Ejercicio Promedio de Notas Versión 2

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -4,18 +4,34 @@ calcularPromedioNotas=function(){
     let nota3;
     let promedio;
     let resultado;
+    let reprobado;
+    let buenTrabajo;
+    let excelente;
+    let datosIncorrectos;
     nota1=recuperarFloat("nota1");
     nota2=recuperarFloat("nota2");
     nota3=recuperarFloat("nota3");
 
     promedio=calcularPromedio(nota1,nota2,nota3);
     resultado=promedio.toFixed(2);
+    reprobado="REPROBADO"
+    buenTrabajo="BUEN TRABAJO"
+    excelente="EXCELENTE"
+    datosIncorrectos="DATOS INCORRECTOS"
 
     mostrarTexto("promedio",resultado);
     
-    if(promedio>7){
-        mostrarImagen("imagenNotas","./imagenes/soyExito.gif");
-    }else{
-        mostrarImagen("imagenNotas","./imagenes/fracaso.gif");
+    if(promedio<5 && promedio>0){
+        mostrarImagen("imagenNotas","./imagenes/reprobado.gif");
+        mostrarTexto("texto",reprobado);
+    }else if(promedio>=5 && promedio<=8){
+        mostrarImagen("imagenNotas","./imagenes/buenTrabajo.gif");
+        mostrarTexto("texto",buenTrabajo);
+    }else if(promedio>8 && promedio<=10){
+        mostrarImagen("imagenNotas","./imagenes/excelente.gif");
+        mostrarTexto("texto",excelente);
+    }else if(promedio>10){
+        mostrarImagen("imagenNotas","./imagenes/error.gif");
+        mostrarTexto("texto",datosIncorrectos);
     }
 }
