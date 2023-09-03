c1d0dfb45308036dbdf4a504050161b64ac7c3ad
ROSEMENESE <dianavillacis05@outlook.es>
2023-09-03 18:44:07 -0500

    RETO 37: EJERCICIOS FOR

---
+++ bucles/ejemplosFor.js
@@ -0,0 +1,46 @@
+mostrarNumeros=function(){
+    console.log("antes del for");
+    for(let i=0;i<4;i++){
+        console.log(i);
+    }
+
+    console.log("despues del for");
+}
+
+mostrarNumeros2=function(){
+    console.log("antes del for");
+    for(let indice=1;indice<=5;indice++){
+        console.log(indice);
+    }
+    console.log("despues del for");
+}
+mostrarPares=function(){
+    console.log("antes del for");
+    for(let x=2;x<10;x+=2){
+        console.log(x);
+
+    }
+    console.log("despues del for");
+}
+mostrarInverso=function(){
+    console.log("antes del for");
+    for(let i=10;i>0;i--){
+        console.log(i);
+    }
+    console.log("despues del for");
+}
+hackearNasaPelis=function(){
+    // hackeando nasa 0% for...100%
+    for(let porcentaje=0;porcentaje<=100;porcentaje+=10){
+        console.log("hackeando nasa"+porcentaje+"%");
+    }
+    console.log("la nasa ha sido hackeada XD");
+}
+
+mostrarImpares=function(){
+    console.log("antes del for");
+    for(i=1;i<=21;i+=2){
+        console.log(i);
+    }
+    console.log("despues del for");
+}
