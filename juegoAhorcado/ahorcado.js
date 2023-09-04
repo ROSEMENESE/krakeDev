82e793e7daeb6bc621ce0a6b536c357f6197b0f2
ROSEMENESE <dianavillacis05@outlook.es>
2023-09-04 16:28:31 -0500

    PASO 1 COMPLETADO

--- juegoAhorcado/ahorcado.js
+++ juegoAhorcado/ahorcado.js
@@ -1,4 +1,5 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+let palabraSecreta=" ",
 esMayuscula = function (caracter) {
     caracter = caracter.toString();
     let letra;
@@ -9,4 +10,24 @@ esMayuscula = function (caracter) {
         return false;
     }
 
+}
+guardarPalabra = function () {
+    let palabra = recuperarTexto("txtSecreta");
+    valido=false;
+    if (palabra.length == 5){
+        let mayusculas=true;
+        for(let i=0;i<palabra.length;i++){
+            if(!esMayuscula(palabra.charAt(i))){
+                mayusculas=false;
+            }
+        }
+        if(mayusculas){
+        palabraSecreta=palabra;
+        console.log(palabraSecreta);
+        }else{
+            alert("SOLO LETRAS MAYUSCULAS")
+        }
+    }else{
+        alert("DEBE INGRESAR UNA PALABRA DE 5 LETRAS MAYUSCULAS");
+    }
 }
