19cdadf95fde10d20a329af1ebe261d17762d788
ROSEMENESE <dianavillacis05@outlook.es>
2023-09-04 22:21:18 -0500

    PASO 7 COMPLETADO

--- juegoAhorcado/ahorcado.js
+++ juegoAhorcado/ahorcado.js
@@ -63,9 +63,9 @@ ingresarLetra=function(){
     validar(letra);
     if(esMayuscula(letra)){
         if(coincidencias==5){
-            alert("HA GANADO");
+            mostrarImagen("ahorcadoImagen","ganador.gif");
         }else if(intentos== 10){
-            alert("HA PERDIDO");
+            mostrarImagen("ahorcadoImagen","gameOver.gif");
         }
     }else{
         alert("SOLO SE ACEPTAN MAYUSCULAS");
