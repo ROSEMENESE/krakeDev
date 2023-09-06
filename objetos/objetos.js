4487f325dcf253bc3f7973f819eadaf1e824e4b6
ROSEMENESE <dianavillacis05@outlook.es>
2023-09-05 23:44:46 -0500

    RETO 44: CLIENTES

--- objetos/objetos.js
+++ objetos/objetos.js
@@ -35,3 +35,54 @@ crearProducto=function(){
         console.log("Producto 2 tiene mayor stock");
     }
 }
+
+modificarAtributos=function(){
+    let cuenta ={
+        numero:"146432165321313",
+        saldo: 0.0
+    }
+    cuenta.saldo=100;
+    cuenta.saldo+=10;
+    console.log(cuenta.saldo);
+}
+
+crearCliente=function(){
+    let cliente={
+        cedula:"1753593467",
+        nombre:"Juan"
+    }
+    let cliente1={}
+        cliente1.nombre="Romeo";
+        cliente1.apellido="Salcedo";
+        cliente1.cedula="1424217893"
+    }
+probarIncrementoSaldo=function(){
+    let cta={numero:"23424",saldo:34.0};
+    incrementarSaldo(cta,100);
+    console.log(cta.saldo);
+}
+
+probarDeterminarMayor=function(){
+    let per1={nombre:"Daniel",edad:45};
+    let per2={nombre:"Luisa",edad:48};
+    let mayor;
+    mayor=determinarMayor(per1,per2);
+    if(mayor != null){
+        console.log("El mayor es:"+" "+mayor.nombre);
+    }
+}
+
+incrementarSaldo=function(cuenta,monto){
+    cuenta.saldo+=monto;
+}
+
+
+determinarMayor=function(persona1,persona2){
+    if(persona1.edad>persona2.edad){
+        return persona1;
+    }else if(persona2.edad>persona1.edad){
+        return persona2;
+    }else{
+        return null;
+    }
+}
